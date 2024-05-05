0349a7e37c94fe5ad4373b316bca7725edb82115
mdaftab20089 <aftabrahi20089@gmail.com>
2024-05-05 22:03:39 +0530

    Zig-zag-Traversal

--- LevelOrder.java
+++ LevelOrder.java
@@ -71,6 +71,44 @@ class CBT2 {
       } 
       return list;
       }
+    public ArrayList<ArrayList<Integer>> ZigZagTraversal(Node root) {
+      ArrayList<ArrayList<Integer>> list =new ArrayList<>();
+      if(root == null) {
+        return list;
+      }
+      Deque<Node> dq=new LinkedList<>();
+      dq.offer(root);
+      boolean rev=false;
+      while(!dq.isEmpty()) {
+      int levelSize=dq.size();
+      ArrayList<Integer> curr=new ArrayList<>();
+      for(int i=0; i<levelSize; i++) {
+        if(!rev) {
+          Node temp=dq.pollFirst();
+          curr.add(temp.data);
+          if(temp.left!=null) {
+            dq.offerLast(temp.left);
+          }
+          if(temp.right!=null) {
+            dq.offerLast(temp.right);
+          }
+        } else {
+          Node temp=dq.pollLast();
+          curr.add(temp.data);
+          if(temp.right!=null) {
+            dq.offerFirst(temp.right);
+          }
+          if(temp.left!=null) {
+            dq.offerFirst(temp.right);
+          }
+        }
+
+      }
+      rev=!rev;
+      list.add(curr);
+      }
+      return list;
+    }  
     }
 
 
